6f37c928273d14f6c885edfc7ba6e2d41bed1177
Gauravbharti1998 <gaurav.bharti1998@gmail.com>
2023-10-29 02:53:23 +0530

    final css along with target achieved popup

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [prev, newVal] = useState(0);
+  const [target, newVal] = useState(0);
   const setTarget = () => {
     const val = document.getElementById("dt").value;
     newVal(val);
   }
-
   const [stats, setStats] = useState(0);
   const increase = () => {
     setStats(stats + 1);
+    if( stats >= target-1 ){ 
+      document.getElementById('congrats').style.visibility='visible';
+      document.getElementById('congrats').innerHTML = 'Congratulations!! </br> You have achieved your daily water intake goal !';
+    }
   }
   const decrease = () => {
     if (stats > 0) {
@@ -23,17 +26,17 @@ function App() {
       <div className='heading'>
         <h1>DAILY WATER GOAL TRACKER</h1>
       </div>
-      <div className='body'>
-        
+      <div className='body'>       
           <div className='upperbdy'>
             <div className='upperbdy1'>
-              <h2>Current Daily target :- <strong>{prev}</strong> Bottles</h2>
+              <h2>Current Daily target :- <strong>{target}</strong> Bottles</h2>
             </div>
             <div className='upperbdy2'>
               <h3>Set daily target</h3>
               <input type='number' id='dt'></input>
               <button className='set' type='button' onClick={setTarget}>SET</button>
             </div>
+            <div className='congo' id = "congrats"> </div>
           </div>
           <div className='countr'>
             <h2>Increase or Decrease the count of bottles consumed.</h2>
