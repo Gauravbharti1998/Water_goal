68d6a0834c6a71347c88f20b117a418bb638a744
Gauravbharti1998 <gaurav.bharti1998@gmail.com>
2023-10-29 14:13:20 +0530

    Added the total volume consumed functionality

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@
 import './App.css';
 import { useState } from 'react';
 
+
 function App() {
   const [target, newVal] = useState(0);
   const setTarget = () => {
     const val = document.getElementById("dt").value;
     newVal(val);
   }
+
   const [stats, setStats] = useState(0);
   const increase = () => {
     setStats(stats + 1);
     if( stats >= target-1 ){ 
+      var temp = document.getElementById("dt2").value;
       document.getElementById('congrats').style.visibility='visible';
-      document.getElementById('congrats').innerHTML = 'Congratulations!! </br> You have achieved your daily water intake goal !';
+      document.getElementById('congrats').innerHTML = 'Congratulations!! </br> You have achieved your daily water intake goal !<br/> Total water intake  ' + '</br>' +(temp * target)/1000 + ' Litres';
     }
   }
   const decrease = () => {
@@ -21,11 +24,13 @@ function App() {
       setStats(stats - 1)
     }
   }
+ 
   return (
     <div>
       <div className='heading'>
         <h1>DAILY WATER GOAL TRACKER</h1>
       </div>
+      <h4>NOTE: This app keeps track considering that you use same volume of bottle everytime.</h4>
       <div className='body'>       
           <div className='upperbdy'>
             <div className='upperbdy1'>
@@ -35,6 +40,11 @@ function App() {
               <h3>Set daily target</h3>
               <input type='number' id='dt'></input>
               <button className='set' type='button' onClick={setTarget}>SET</button>
+            </div>
+            <div className='vol'>
+                <h3> Enter bottle volume in ml.</h3>
+                <input type = 'number' id='dt2'></input><br/>
+                <button className='save' type = 'button' > SAVE</button>
             </div>
             <div className='congo' id = "congrats"> </div>
           </div>
